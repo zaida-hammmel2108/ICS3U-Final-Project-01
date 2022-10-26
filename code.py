0a2a20be4fadedd2c5ccf41013d9e0394c28e48e
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-10-26 11:20:01 -0400

    edited code

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This program is teh "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+def  game_scene():
+    # this function is the main game game scene
+    
+    print("\n\n\n") # 3 blank spaces
+    print("Hello, World!")
+
+    # repeat forever, game loop
+    while True:
+        pass #just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene():
