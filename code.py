87c84746acfabc3a9c9e1418136e19e0d4cd9220
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-10-31 11:22:51 -0400

    added sprite code

--- code.py
+++ code.py
@@ -11,26 +11,39 @@ import stage
 def game_scene():
     # this function is the main game game_scene
 
-    # image banks for CircuitPython
-    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+# image banks for CircuitPython
+image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
 
-    # set the background to image 0 in the image bank
-    #  and the size (10x8 tiles of size 16x16)
-    background = stage.Grid(image_bank_background, 10, 8)
+# set the background to image 0 in the image bank
+#  and the size (10x8 tiles of size 16x16)
+background = stage.Grid(image_bank_background, 10, 8)
 
-    # Create a stage for the background to show up on
-    # and set the frame rate to 60fps
-    game = stage.Stage(ugame.display, 60)
-    # set the layers of all sprites, items show up in order
-    game.layers = [background]
-    # render all sprites
-    #   most likely you will only render the background once per game scene
-    game.render_block()
+# a sprite that will be updated every frame
+ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+
+# Create a stage for the background to show up on
+#   and set the frame rate to 60fps
+game = stage.Stage(ugame.display, 60)
+
+# set the layers of all sprites, items show up in order
+game.layers = [ship] + [background]
+
+# render all sprites
+#   most likely you will only render the background once per game scene
+game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass # just a placeholder for now
+        # get user input
+
+        # update game logic
+
+        # redraw Sprite
+        game.render_sprites([ship])
+        game.tick() # wait until refresh rate finishes
+
 
 if __name__ == "__main__":
     game_scene()
