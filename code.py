e8e12097846a4424a3cdd3e65f6b83a5a5bccbd0
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-10-31 11:18:05 -0400

    added background

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 # Created by: Zaida Hammel
 # Created on: Oct 2022
 # This program is the "Space Aliens" program on the PyBadge
@@ -7,15 +8,29 @@
 import ugame
 import stage
 
-def  game_scene():
-    # this function is the main game game scene
-    
-    print("\n\n\n") # 3 blank spaces
-    print("Hello, World!")
+def game_scene():
+    # this function is the main game game_scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+
+    # set the background to image 0 in the image bank
+    #  and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # Create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render all sprites
+    #   most likely you will only render the background once per game scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
-        pass #just a placeholder for now
+        pass # just a placeholder for now
 
 if __name__ == "__main__":
     game_scene()
