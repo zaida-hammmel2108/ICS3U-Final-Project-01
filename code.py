55bd102b7ec5a7b30d555354b5c9cd4352306d1e
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-11-02 11:22:28 -0400

    added movement

--- code.py
+++ code.py
@@ -11,32 +11,50 @@ import stage
 def game_scene():
     # this function is the main game game_scene
 
-# image banks for CircuitPython
-image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
-image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
 
-# set the background to image 0 in the image bank
-#  and the size (10x8 tiles of size 16x16)
-background = stage.Grid(image_bank_background, 10, 8)
+    # set the background to image 0 in the image bank
+    #  and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
 
-# a sprite that will be updated every frame
-ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # a sprite that will be updated every frame
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
-# Create a stage for the background to show up on
-#   and set the frame rate to 60fps
-game = stage.Stage(ugame.display, 60)
+    # Create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
 
-# set the layers of all sprites, items show up in order
-game.layers = [ship] + [background]
+    # set the layers of all sprites, items show up in order
+    game.layers = [ship] + [background]
 
-# render all sprites
-#   most likely you will only render the background once per game scene
-game.render_block()
+    # render all sprites
+    #   most likely you will only render the background once per game scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
+
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x,ship.y + 1)
 
         # update game logic
 
