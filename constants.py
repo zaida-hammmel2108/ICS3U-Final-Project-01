7d608f65474d6fa3b5ec4d64b7f8d8e59e213095
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-11-08 10:46:29 -0500

    added constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Zaida Hammel
+# Created on: Oct 2022
+# This constants file is for Space Aliens game
+
+# PyBadge screen size 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
