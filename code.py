eeeb705336336f5a4e7a1ce07fee633e1a2638ab
Zaida Hammel <99354763+zaida-hammmel2108@users.noreply.github.com>
2022-12-06 11:00:38 -0500

    edited code

--- code.py
+++ code.py
@@ -6,19 +6,81 @@
 
 
 import constants
-import ugame
 import stage
+import ugame
 
 
-def menu_scene():
-    # this function is the menu_scene
+def splash_scene():
+    # this function is the splash scene
+
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
 
     # image banks for CircuitPython
-    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # set the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    # used this program to split the image into tile: 
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    # create a stage for the background to show up on
+    #  and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    # render the background and initial lcation of sprite list
+    # most likely you will only render the background once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # Wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
+
+def menu_scene():
+    # this function is the main game game scene
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
 
     # add text objects
     text = []
-    text1 = stage.Text(width=29, height=12, dont=None, palette=constants.RED_PALETTE, buffer=None)
+    text1 = stage.Text(width=29, height=12, font=None, palette=constants.RED_PALETTE, buffer=None)
     text1.move(20,10)
     text1.text("MT Game Studios")
     text.append(text1)
@@ -32,13 +94,14 @@ def menu_scene():
     background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
                             constants.SCREEN_Y)
 
+
     # create a stage for the background to show up on
-    #   and set the frame rate to 60fps
+    #  and set the frame rate to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
     # set the layers of all sprites, items show up in order
     game.layers = text + [background]
-    # render all sprites
-    # most likely you will only render the background once per game scene
+    # render the background and initial lcation of sprite list
+    # most likely you will only render the background once per scene
     game.render_block()
 
     # repeat forever, game loop
@@ -46,21 +109,21 @@ def menu_scene():
         # get user input
         keys = ugame.buttons.get_pressed()
 
-        # Start button selected
-        if keys & ugame.K_START != 0
-            game_scene()
-
-        # update game logic
-        game.tick() # wait until refresh rate finishes
+        # A button to fire
+        if keys & ugame.K_START:
+            print("Start")
+# update game logic
+        # redraw Sprites
+        game.tick()  # wait until refresh rate finishes
 
 def game_scene():
-    # this function is the main game game_scene
+    # this function is the main game game scene
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
-    # buttons that you want to keep state information on
+    # buttons that you want to keep state nformation on
     a_button = constants.button_state["button_up"]
     b_button = constants.button_state["button_up"]
     start_button = constants.button_state["button_up"]
@@ -73,13 +136,17 @@ def game_scene():
     sound.mute(False)
 
     # set the background to image 0 in the image bank
-    #  and the size (10x8 tiles of size 16x16)
+    #  and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y)
+            tile.picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
 
     # a sprite that will be updated every frame
     ship = stage.Sprite(
-        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE
-) )
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
 
     alien = stage.Sprite(
         image_bank_sprites,
@@ -89,14 +156,14 @@ def game_scene():
     )
 
     # create a stage for the background to show up on
-    #   and set the frame rate to 60fps
+    #  and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
 
     # set the layers of all sprites, items show up in order
     game.layers = [ship] + [alien] + [background]
 
     # render all sprites
-    #   most likely you will only render the background once per game scene
+    # most likely you will only render the background once per game scene
     game.render_block()
 
     # repeat forever, game loop
@@ -116,27 +183,35 @@ def game_scene():
             else:
                 a_button = constants.button_state["button_up"]
 
+        if keys & ugame.K_O:
+            pass
+        if keys & ugame.K_START:
+            pass
+        if keys & ugame.K_SELECT:
+            pass
         if keys & ugame.K_RIGHT != 0:
-            if ship.x < (constants.SCREEN_X - constants.SPRITE_SIZE):
-                ship.move((ship.x + constants.SPRITE_MOVEMENT_SPEED), ship.y)
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+                ship.move(ship.x + 1, ship.y)
             else:
-                    ship.move((constants.SCREEN_X - constants.SPRITE_SIZE), ship.y)
-             
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
         if keys & ugame.K_LEFT != 0:
-            if ship.x > 0:
-                ship.move((ship.x - constants.SPRITE_MOVEMENT_SPEED), ship.y)
-            else:
-                    ship.move(0, ship.y)
+            if ship.x >= 0:
+                ship.move(ship.x - 1, ship.y)
+        else:
+            ship.move(0, ship.y)
+        if keys & ugame.K_UP:
+            pass
+        if keys & ugame.K_DOWN:
+            pass
 
         # update game logic
         # play sound if A was just button_just_pressed
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
 
-        # redraw Sprite
+        # redraw Sprites
         game.render_sprites([ship] + [alien])
-        game.tick() # wait until refresh rate finishes
-
+        game.tick()  # wait until refresh rate finishes
 
 if __name__ == "__main__":
     menu_scene()
--- constants.py
+++ constants.py
@@ -10,7 +10,6 @@ SCREEN_Y = 128
 SCREEN_GRID_X = 10
 SCREEN_GRID_Y = 8
 SPRITE_SIZE = 16
-TOTAL_NUMBER_OF_ALIENS = 5
 FPS = 60
 SPRITE_MOVEMENT_SPEED = 1
 
@@ -21,6 +20,9 @@ button_state = {
     "button_still_pressed": "still pressed",
     "button_released": "released",
 }
-# new palette for red filled text
-RED_PALETTE = (b'\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff'
-               b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff')
+
+# new pallet for red filled text
+RED_PALETTE = (
+    b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+    b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
+)
